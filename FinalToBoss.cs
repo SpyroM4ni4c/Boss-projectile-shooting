271a6d15ec1fbd2bbfbffaedf0627996f1e0430b
Archorr <64887716+SpyroM4ni4c@users.noreply.github.com>
2020-05-06 09:33:44 +0300

    Add files via upload
    These 2 scripts make the boss look at the target position and then instantiate and shoot a projectile to the last registered position before it instantiates

---
+++ FinalToBoss.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalToBoss : MonoBehaviour
+{
+    public GameObject player;
+    public Animator chandelier;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            
+            chandelier.SetBool("isMoving", true);
+        }
+    }
+
+
+}
